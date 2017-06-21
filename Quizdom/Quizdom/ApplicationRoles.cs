56ccdd7f40db7e2afd3099eeb9721e88cebab272
Rick Cohen <rickco@gmail.com>
2017-06-21 19:08:48 -0400

    Updated File Structure

---
+++ Quizdom/Quizdom/ApplicationRoles.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Linq;
+
+namespace Quizdom
+{
+    public class ApplicationRoles
+    {
+        private RoleManager<IdentityRole> roleManager;
+
+        public ApplicationRoles(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public void CreateRoles()
+        {
+            GenerateRole("Admin");
+            GenerateRole("Normal");
+        }
+
+        private void GenerateRole(string roleName)
+        {
+            var hasRole = roleManager.Roles.FirstOrDefault(x => x.NormalizedName.Equals(roleName.ToUpperInvariant()));
+            if (hasRole == null)
+            {
+                roleManager.CreateAsync(new IdentityRole(roleName)).Wait();
+            }
+        }
+    }
+}

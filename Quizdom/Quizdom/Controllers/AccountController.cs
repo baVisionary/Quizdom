db3910d12cd569112a73801426c13cf17d4dd54d
Rick Cohen <rickco@gmail.com>
2017-06-22 18:37:26 -0400

    Changed to MySQL Database

--- Quizdom/Quizdom/Controllers/AccountController.cs
+++ Quizdom/Quizdom/Controllers/AccountController.cs
@@ -122,14 +122,15 @@ namespace Quizdom.Controllers
                     //var callbackUrl = Url.Action(nameof(ConfirmEmail), "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
                     //await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
                     //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    _logger.LogInformation(3, "User created a new account with password.");
 
                     await _userManager.AddToRoleAsync(user, "Normal");
                     if (user.Email.Contains("rickco@gmail.com"))
                     {
                         await _userManager.AddToRoleAsync(user, "Admin");
                     }
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    _logger.LogInformation(3, "User created a new account with password.");
 
                     return RedirectToLocal(returnUrl);
                 }

92e23e33be3e81c70e332ddb0309a7c1c103bbed
Rick Cohen <rickco@gmail.com>
2017-06-29 15:57:12 -0400

    Added Avatars and Categories Table to DB

--- Quizdom/Quizdom/Data/Migrations/20170629194948_addedAvatarAndCategoryTables.Designer.cs
+++ Quizdom/Quizdom/Data/Migrations/20170629194948_addedAvatarAndCategoryTables.Designer.cs
@@ -8,9 +8,10 @@ using Quizdom.Data;
 namespace Quizdom.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170629194948_addedAvatarAndCategoryTables")]
+    partial class addedAvatarAndCategoryTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2");

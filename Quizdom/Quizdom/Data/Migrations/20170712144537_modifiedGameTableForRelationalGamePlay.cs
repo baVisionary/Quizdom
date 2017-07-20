d64f6193803fa33208a2dd5ded324ce24352849a
Davin Nathanson <dtnathanson@gmail.com>
2017-07-20 14:43:53 -0700

    fixed not-deleted Data migrations

---
+++ Quizdom/Quizdom/Data/Migrations/20170712144537_modifiedGameTableForRelationalGamePlay.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quizdom.Data.Migrations
+{
+    public partial class modifiedGameTableForRelationalGamePlay : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Categories",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "InvitedUserEmails",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Players",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "Questions",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "QuestionsState",
+                table: "Games");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GameLength",
+                table: "Games",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Categories",
+                table: "Games",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InvitedUserEmails",
+                table: "Games",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Players",
+                table: "Games",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Questions",
+                table: "Games",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "QuestionsState",
+                table: "Games",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GameLength",
+                table: "Games",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}

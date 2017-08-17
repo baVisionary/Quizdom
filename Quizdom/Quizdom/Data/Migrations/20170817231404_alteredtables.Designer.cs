b63269cc6b1b76a5683a9a85bd985d8258ad0c98
Rick Cohen <rickco@gmail.com>
2017-08-17 19:15:34 -0400

    database migration

--- Quizdom/Quizdom/Data/Migrations/20170817231404_alteredtables.Designer.cs
+++ Quizdom/Quizdom/Data/Migrations/20170817231404_alteredtables.Designer.cs
@@ -8,9 +8,10 @@ using Quizdom.Data;
 namespace Quizdom.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170817231404_alteredtables")]
+    partial class alteredtables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2");

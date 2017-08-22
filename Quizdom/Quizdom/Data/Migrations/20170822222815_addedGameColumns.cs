f0fe14fb0433cad304d65f9a3b8d18c01024e6d8
Rick Cohen <rickco@gmail.com>
2017-08-22 18:31:43 -0400

    added new columns

---
+++ Quizdom/Quizdom/Data/Migrations/20170822222815_addedGameColumns.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quizdom.Data.Migrations
+{
+    public partial class addedGameColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "gameBoardId",
+                table: "Games",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "gameState",
+                table: "Games",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "gameBoardId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "gameState",
+                table: "Games");
+        }
+    }
+}

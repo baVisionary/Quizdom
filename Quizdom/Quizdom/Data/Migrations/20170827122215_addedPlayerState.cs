0aeacd1d4e6f73c65fbe163007403920e5814e12
Rick Cohen <rickco@gmail.com>
2017-08-27 08:23:02 -0400

    added playerState to gameplayers table

---
+++ Quizdom/Quizdom/Data/Migrations/20170827122215_addedPlayerState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quizdom.Data.Migrations
+{
+    public partial class addedPlayerState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "playerState",
+                table: "GamePlayers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "playerState",
+                table: "GamePlayers");
+        }
+    }
+}

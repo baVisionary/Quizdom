274e86b6dc0d6b505dce4f95967cac526b6005cc
Rick Cohen <rickco@gmail.com>
2017-09-07 14:18:16 -0400

    added player stats fields and new table

---
+++ Quizdom/Quizdom/Models/PlayerStat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quizdom.Models
+{
+    public class PlayerStat
+    {
+        public int Id { get; set; }
+        public string userName { get; set; }
+        public int questionsWon { get; set; }
+        public int questionsRight { get; set; }
+        public int questionsRightDelay { get; set; }
+    }
+}
